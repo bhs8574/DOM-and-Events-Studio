f49061a35409f0c4ad1adeabc678df00c0bd2a4c
bhs8574 <bhs8574@gmail.com>
2020-03-12 19:09:34 -0500

    Implemented through step 2, I believe.

--- scripts.js
+++ scripts.js
@@ -1,2 +1,15 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+window.addEventListener("load", function() {
+    let takeOffButton = document.getElementById("takeoff");
+    takeOffButton.addEventListener("click", function (event) {
+        let launchConfirm = confirm("Confirm that the shuttle is ready for takeoff.");
+        if (launchConfirm) {
+            document.getElementById("flightStatus").innerText = "Shuttle in flight.";
+            document.getElementById("shuttleBackground").setAttribute("style", "background-color: blue;");
+            let spaceShuttleHeight = Number(document.getElementById("spaceShuttleHeight").innerText);
+            spaceShuttleHeight += 10000;
+            document.getElementById("spaceShuttleHeight").innerText = `${spaceShuttleHeight}`;
+        }
+    });
+});
