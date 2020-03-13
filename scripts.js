25246c14bcb967444a3ad827dc2720935612e06d
bhs8574 <bhs8574@gmail.com>
2020-03-12 21:58:56 -0500

    Added step 5. Bonuses are next.

--- scripts.js
+++ scripts.js
@@ -4,12 +4,19 @@ window.addEventListener("load", function() {
     let takeOffButton = document.getElementById("takeoff");
     let landButton = document.getElementById("landing");
     let abortButton = document.getElementById("missionAbort");
+    let upButton = document.getElementById("up");
+    let downButton = document.getElementById("down");
+    let leftButton = document.getElementById("left");
+    let rightButton = document.getElementById("right");
+    let spaceShuttleHeight;
+    let horiDistance = 0; 
+    let vertDistance = 0;
     takeOffButton.addEventListener("click", function (event) {
         let launchConfirm = confirm("Confirm that the shuttle is ready for takeoff.");
         if (launchConfirm) {
             document.getElementById("flightStatus").innerText = "Shuttle in flight.";
             document.getElementById("shuttleBackground").setAttribute("style", "background-color: blue;");
-            let spaceShuttleHeight = Number(document.getElementById("spaceShuttleHeight").innerText);
+            spaceShuttleHeight = Number(document.getElementById("spaceShuttleHeight").innerText);
             spaceShuttleHeight += 10000;
             document.getElementById("spaceShuttleHeight").innerText = `${spaceShuttleHeight}`;
         }
@@ -28,5 +35,29 @@ window.addEventListener("load", function() {
             document.getElementById("spaceShuttleHeight").innerText = "0";
         }
     });
-    
+    upButton.addEventListener("click", function (event) {
+        vertDistance -= 10;
+        document.getElementById("rocket").setAttribute("style", `top: ${vertDistance}px; right: ${horiDistance}px;`);
+        spaceShuttleHeight = Number(document.getElementById("spaceShuttleHeight").innerText);
+        spaceShuttleHeight += 10000;
+        document.getElementById("spaceShuttleHeight").innerText = `${spaceShuttleHeight}`;
+    });
+    downButton.addEventListener("click", function (event) {
+        vertDistance += 10;
+        document.getElementById("rocket").setAttribute("style", `top: ${vertDistance}px; right: ${horiDistance}px;`);
+        spaceShuttleHeight = Number(document.getElementById("spaceShuttleHeight").innerText);
+        spaceShuttleHeight -= 10000;
+        if (spaceShuttleHeight <= 0) {
+            spaceShuttleHeight = 0;
+        }
+        document.getElementById("spaceShuttleHeight").innerText = `${spaceShuttleHeight}`;
+    });
+    leftButton.addEventListener("click", function (event) {
+        horiDistance += 10;
+        document.getElementById("rocket").setAttribute("style", `top: ${vertDistance}px; right: ${horiDistance}px;`);
+    });
+    rightButton.addEventListener("click", function (event) {
+        horiDistance -= 10;
+        document.getElementById("rocket").setAttribute("style", `top: ${vertDistance}px; right: ${horiDistance}px;`);
+    });
 });
