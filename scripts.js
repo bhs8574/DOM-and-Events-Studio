535e1cb7e2a25a799815551e33ab67a9b99c333c
bhs8574 <bhs8574@gmail.com>
2020-03-12 20:11:00 -0500

    Added steps 3 and 4.

--- scripts.js
+++ scripts.js
@@ -2,6 +2,8 @@
 // Remember to pay attention to page loading!
 window.addEventListener("load", function() {
     let takeOffButton = document.getElementById("takeoff");
+    let landButton = document.getElementById("landing");
+    let abortButton = document.getElementById("missionAbort");
     takeOffButton.addEventListener("click", function (event) {
         let launchConfirm = confirm("Confirm that the shuttle is ready for takeoff.");
         if (launchConfirm) {
@@ -12,4 +14,19 @@ window.addEventListener("load", function() {
             document.getElementById("spaceShuttleHeight").innerText = `${spaceShuttleHeight}`;
         }
     });
+    landButton.addEventListener("click", function (event) {
+        alert("The shuttle is landing. Landing gear engaged!");
+        document.getElementById("flightStatus").innerText = "The shuttle has landed.";
+        document.getElementById("shuttleBackground").setAttribute("style", "background-color: green;");
+        document.getElementById("spaceShuttleHeight").innerText = "0";
+    });
+    abortButton.addEventListener("click", function (event) {
+        let abortConfirm = confirm("Confirm that you want to abort the mission.");
+        if (abortConfirm) {
+            document.getElementById("flightStatus").innerText = "Mission aborted.";
+            document.getElementById("shuttleBackground").setAttribute("style", "background-color: green;");
+            document.getElementById("spaceShuttleHeight").innerText = "0";
+        }
+    });
+    
 });
